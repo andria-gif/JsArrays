664dbe99c49382d9de175c91618cced6b51a12ff
Ândria Rosa Dias <82342320+andria-gif@users.noreply.github.com>
2023-07-13 16:27:42 -0300

    Add files via upload

---
+++ deletandoElementos.js
@@ -0,0 +1,12 @@
+const notas = [10, 6, 8, 5.5, 10];
+
+notas.pop();//para remover o ultimo elemento
+
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3])/notas.length;
+
+
+console.log(media);
+
+
+
